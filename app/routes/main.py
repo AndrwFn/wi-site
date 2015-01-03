ab11d3c3cd0246840920e83563b3c24980190efb
a_lampcat <fatkulin@a2design.biz>
2015-01-03 20:44:07 +0600

    Changed html template: added static_url

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,10 @@
 import tornado.template
 from .base import BaseHandler
+from pyjade.ext.tornado import patch_tornado
+patch_tornado()
 
 
 class MainHandler(BaseHandler):
 	def get(self):
 		print(self.request)
-		self.write("Hello")
+		self.render("index.html")
